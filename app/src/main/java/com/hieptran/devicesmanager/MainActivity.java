8a99edf902ccd5c6adffa2e15bd6c8eba3f56082
hiepth <hieptran.bka@gmail.com>
2016-01-10 22:35:43 +0700

    Test onBackPress

--- app/src/main/java/com/hieptran/devicesmanager/MainActivity.java
+++ app/src/main/java/com/hieptran/devicesmanager/MainActivity.java
@@ -1,16 +1,14 @@
 package com.hieptran.devicesmanager;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -19,6 +17,8 @@ import com.hieptran.devicesmanager.fragment.device.DeviceFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    boolean pressAgain = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +36,10 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
-     boolean pressAgain = true;
+
     @Override
     public void onBackPressed() {
-
+        Log.d("HiepTHb", "onBackPressed");
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
