91bdb1d69884142d7c8ac97ec4b36b0275c6916b
hiepgaf <hiepgaf@github.com>
2016-01-13 10:23:21 +0700

    Dang xu ly CPU Fragment

--- app/src/main/java/com/hieptran/devicesmanager/fragment/tweak/cpu/CPUFragment.java
+++ app/src/main/java/com/hieptran/devicesmanager/fragment/tweak/cpu/CPUFragment.java
@@ -6,37 +6,42 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.SwitchCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
-import android.widget.Spinner;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.hieptran.devicesmanager.R;
 import com.hieptran.devicesmanager.common.CircleChart;
+import com.hieptran.devicesmanager.utils.Utils;
 import com.hieptran.devicesmanager.utils.tweak.CPU;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import comboseekbar.ComboSeekBar;
 import me.itangqi.waveloadingview.WaveLoadingView;
 
 
 /**
  * Created by hieptran on 11/01/2016.
  */
-public class CPUFragment extends Fragment {
+public class CPUFragment extends Fragment implements SeekBar.OnSeekBarChangeListener {
     TextView tvmax, tvmin;
-    Spinner dseekbarmax;
-    Spinner dseekbarmin;
-    ArrayAdapter<String> freqs_adapter;
+    ComboSeekBar mSliderFreqs_max;
+    ComboSeekBar mSliderFreqs_min;
+    List<String> ls_freqs = new ArrayList<>();
     private SwitchCompat[] mCoreCheckBox;
     private CircleChart[] mCoreUsageText;
     private AppCompatTextView[] mCoreFreqText;
@@ -108,6 +113,12 @@ public class CPUFragment extends Fragment {
         }
     };
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     private LinearLayout coreInit(LayoutInflater inflater, ViewGroup container) {
         LinearLayout layout = new LinearLayout(getActivity());
         layout.setPadding(16, 16, 16, 16);
@@ -145,12 +156,12 @@ public class CPUFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         handler = new Handler();
+        getFreqLs();
         LinearLayout layout = new LinearLayout(getActivity());
         layout.setPadding(16, 16, 16, 16);
         layout.setOrientation(LinearLayout.VERTICAL);
         View v = inflater.inflate(R.layout.cpu_tw, container, false);
         ScrollView scrollView = (ScrollView) v.findViewById(R.id.cpu_scrollView);
-
         layout.addView(coreInit(inflater, container));
         layout.addView(sliderBarinit(inflater, container));
         scrollView.addView(layout);
@@ -182,14 +193,10 @@ public class CPUFragment extends Fragment {
         view_max.setBackgroundColor(Color.LTGRAY);
         view_min.setBackgroundColor(Color.LTGRAY);
         tvmax = (TextView) view_max.findViewById(R.id.slider_bar_text);
-
+        mSliderFreqs_max = (ComboSeekBar) view_max.findViewById(R.id.freqs_slider_bar);
+        mSliderFreqs_min = (ComboSeekBar) view_min.findViewById(R.id.freqs_slider_bar);
         tvmin = (TextView) view_min.findViewById(R.id.slider_bar_text);
-        getFreqLs();
-        dseekbarmax = (Spinner) view_max.findViewById(R.id.m_spinner);
-        dseekbarmax.setAdapter(freqs_adapter);
-        dseekbarmin = (Spinner) view_min.findViewById(R.id.m_spinner);
-        dseekbarmin.setAdapter(freqs_adapter);
-
+        setFreqsView();
         layout.addView(view_max);
         layout.addView(inflater.inflate(R.layout.div_view, container, false));
         layout.addView(view_min);
@@ -197,16 +204,70 @@ public class CPUFragment extends Fragment {
     }
 
     private void getFreqLs() {
-        List<String> freqs = new ArrayList<>();
+
         for (int freq : CPU.getFreqs()) {
-            freqs.add(freq / 1000 + getString(R.string.mhz));
-            //freqs_adapter.add(freq / 1000 + getString(R.string.mhz));
-            Log.d("HiepTHb", freq / 1000 + getString(R.string.mhz));
+            ls_freqs.add(freq / 1000 + "");
         }
-        tvmin.setText("Minimum CPU frequency: " + freqs.get(0));
-        tvmax.setText("Maximum CPU frequency: " + freqs.get(freqs.size() - 1));
-        freqs_adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, freqs);
-        freqs_adapter.setNotifyOnChange(true);
+
+    }
+
+    private void setFreqsView() {
+        tvmin.setText("Minimum CPU frequency: " + CPU.getMinFreq(true));
+        tvmax.setText("Maximum CPU frequency: " + CPU.getMaxFreq(true));
+        mSliderFreqs_max.setAdapter(ls_freqs);
+        mSliderFreqs_max.setMax(ls_freqs.size());
+        mSliderFreqs_max.setOnSeekBarChangeListener(this);
+        //mSliderFreqs_max.setProgress(ls_freqs.indexOf(String.valueOf(CPU.getMaxFreq(true))));
+        mSliderFreqs_min.setAdapter(ls_freqs);
+        mSliderFreqs_min.setMax(ls_freqs.size());
+        mSliderFreqs_min.setOnSeekBarChangeListener(this);
+        //mSliderFreqs_max.setProgress(ls_freqs.indexOf(String.valueOf(CPU.getMinFreq(true))));
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        int value = Utils.stringToInt(ls_freqs.get(seekBar.getProgress()));
+        if (seekBar == mSliderFreqs_max) {
+
+            CPU.setMaxFreq(value * 1000, getActivity());
+            tvmax.setText("Maximum CPU frequency: " + value + " MHz");
+
+        } else {
+            CPU.setMinFreq(value * 1000, getActivity());
+            tvmin.setText("Minimum CPU frequency: " + value + " MHz");
+
+        }
+    }
+
+    //Test - Hien tai chua xu ly dung
+    private int getPos(int val) {
+        int k = 0;
+        for (String i : ls_freqs
+                ) {
+            if (!i.equals(val))
+                k++;
+            else
+                return k;
+        }
+        return k;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.set_on_boot, menu);
+        MenuItem item = menu.findItem(R.id.set_on_boot);
+        SwitchCompat switchCompat = (SwitchCompat) MenuItemCompat.getActionView(item);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     class mOnCheckedChanged implements SwitchCompat.OnCheckedChangeListener {
