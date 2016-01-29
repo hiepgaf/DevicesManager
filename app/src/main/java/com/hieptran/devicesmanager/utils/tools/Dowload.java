4f4dacf0dd97f9cf8459078130b94897c0f80756
hieptran <hieptran.bka@gmail.com>
2016-01-29 11:07:56 +0700

    Thay doi cau truc view

---
+++ app/src/main/java/com/hieptran/devicesmanager/utils/tools/Dowload.java
@@ -0,0 +1,13 @@
+package com.hieptran.devicesmanager.utils.tools;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by hiepth on 28/01/2016.
+ */
+public class Dowload extends AsyncTask<String,Integer,String> {
+    @Override
+    protected String doInBackground(String... params) {
+        return null;
+    }
+}
--- app/src/main/java/com/hieptran/devicesmanager/view/carditems/SwitchCardView.java
+++ app/src/main/java/com/hieptran/devicesmanager/view/carditems/SwitchCardView.java
@@ -1,7 +1,16 @@
 package com.hieptran.devicesmanager.view.carditems;
 
+import android.content.Context;
+
+import com.hieptran.devicesmanager.R;
+import com.hieptran.devicesmanager.common.cardview.CommonCardView;
+
 /**
  * Created by hiepth on 28/01/2016.
  */
-public class SwitchCardView {
+public class SwitchCardView extends CommonCardView{
+    public SwitchCardView(Context context) {
+        super(context, R.layout.switchcompat_cardview);
+
+    }
 }
