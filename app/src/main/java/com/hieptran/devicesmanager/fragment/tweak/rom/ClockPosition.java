e805f613b14a446b11e1652100866511b27577c2
hieptran <hieptran.bka@gmail.com>
2016-04-02 00:21:27 +0700

    Add xposed tweak

---
+++ app/src/main/java/com/hieptran/devicesmanager/fragment/tweak/rom/ClockPosition.java
@@ -0,0 +1,206 @@
+package com.hieptran.devicesmanager.fragment.tweak.rom;
+
+/**
+ * Created by hiepth on 01/04/2016.
+ */
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LayoutInflated;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
+import android.content.Context;
+import android.content.res.Resources;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.view.animation.Animation;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class ClockPosition {
+    public static final String PACKAGE_NAME = "com.android.systemui";
+    private static final String TAG = "XSBM-CenterClock";
+    private static final String CLASS_PHONE_STATUSBAR = "com.android.systemui.statusbar.phone.PhoneStatusBar";
+    private static final String CLASS_TICKER = "com.android.systemui.statusbar.phone.PhoneStatusBar$MyTicker";
+
+    private static ViewGroup mIconArea;
+    private static ViewGroup mRootView;
+    private static LinearLayout mLayoutClock;
+    private static TextView mClock;
+    private static Object mPhoneStatusBar;
+    private static Context mContext;
+    private static int mAnimPushUpOut;
+    private static int mAnimPushDownIn;
+    private static int mAnimFadeIn;
+    private static boolean mClockCentered = false;
+    private static int mClockOriginalPaddingLeft;
+
+    private static void log(String message) {
+        XposedBridge.log(TAG + ": " + message);
+    }
+
+    public static void initPackageResources( final InitPackageResourcesParam resparam, final int type) {
+        try {
+            resparam.res.hookLayout(PACKAGE_NAME, "layout", "super_status_bar", new XC_LayoutInflated() {
+
+                @Override
+                public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
+
+                    mIconArea = (ViewGroup) liparam.view.findViewById(
+                            liparam.res.getIdentifier("system_icon_area", "id", PACKAGE_NAME));
+                    if (mIconArea == null) return;
+
+                    mRootView = (ViewGroup) mIconArea.getParent().getParent();
+                    if (mRootView == null) return;
+
+                    mClock = (TextView) mIconArea.findViewById(
+                            liparam.res.getIdentifier("clock", "id", PACKAGE_NAME));
+                    if (mClock == null) return;
+                    mClockOriginalPaddingLeft = mClock.getPaddingLeft();
+
+                    // inject new clock layout
+                    mLayoutClock = new LinearLayout(liparam.view.getContext());
+                    mLayoutClock.setLayoutParams(new LinearLayout.LayoutParams(
+                            LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+                    mLayoutClock.setGravity(Gravity.CENTER);
+                    mLayoutClock.setVisibility(View.GONE);
+                    mRootView.addView(mLayoutClock);
+                    log("mLayoutClock injected");
+
+                   // prefs.reload();
+                    setClockPosition(type);
+
+                }
+            });
+        } catch (Exception e) {
+            XposedBridge.log(e);
+        }
+    }
+
+    public static void initHandleLoadPackage( final ClassLoader classLoader) {
+        try {
+            final Class<?> phoneStatusBarClass =
+                    XposedHelpers.findClass(CLASS_PHONE_STATUSBAR, classLoader);
+            final Class<?> tickerClass =
+                    XposedHelpers.findClass(CLASS_TICKER, classLoader);
+
+            final Class<?>[] loadAnimParamArgs = new Class<?>[2];
+            loadAnimParamArgs[0] = int.class;
+            loadAnimParamArgs[1] = Animation.AnimationListener.class;
+
+            XposedHelpers.findAndHookMethod(phoneStatusBarClass, "makeStatusBarView", new XC_MethodHook() {
+
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    mPhoneStatusBar = param.thisObject;
+                    mContext = (Context) XposedHelpers.getObjectField(mPhoneStatusBar, "mContext");
+                    Resources res = mContext.getResources();
+                    mAnimPushUpOut = res.getIdentifier("push_up_out", "anim", "android");
+                    mAnimPushDownIn = res.getIdentifier("push_down_in", "anim", "android");
+                    mAnimFadeIn = res.getIdentifier("fade_in", "anim", "android");
+
+                }
+            });
+
+            XposedHelpers.findAndHookMethod(tickerClass, "tickerStarting", new XC_MethodHook() {
+
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    if (mLayoutClock == null || !mClockCentered) return;
+
+                    mLayoutClock.setVisibility(View.GONE);
+                    Animation anim = (Animation) XposedHelpers.callMethod(
+                            mPhoneStatusBar, "loadAnim", loadAnimParamArgs, mAnimPushUpOut, null);
+                    mLayoutClock.startAnimation(anim);
+                }
+            });
+
+            XposedHelpers.findAndHookMethod(tickerClass, "tickerDone", new XC_MethodHook() {
+
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    if (mLayoutClock == null || !mClockCentered) return;
+
+                    mLayoutClock.setVisibility(View.VISIBLE);
+                    Animation anim = (Animation) XposedHelpers.callMethod(
+                            mPhoneStatusBar, "loadAnim", loadAnimParamArgs, mAnimPushDownIn, null);
+                    mLayoutClock.startAnimation(anim);
+                }
+            });
+
+            XposedHelpers.findAndHookMethod(tickerClass, "tickerHalting", new XC_MethodHook() {
+
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    if (mLayoutClock == null || !mClockCentered) return;
+
+                    mLayoutClock.setVisibility(View.VISIBLE);
+                    Animation anim = (Animation) XposedHelpers.callMethod(
+                            mPhoneStatusBar, "loadAnim", loadAnimParamArgs, mAnimFadeIn, null);
+                    mLayoutClock.startAnimation(anim);
+                }
+            });
+        }
+        catch (Exception e) {
+            XposedBridge.log(e);
+        }
+    }
+
+    private static void setClockPosition(int type) {
+        if ( mClock == null ||
+                mIconArea == null || mLayoutClock == null) {
+            return;
+        }
+        switch (type) {
+            case 0:mClock.setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL);
+                mClock.setLayoutParams(new LinearLayout.LayoutParams(
+                        LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT));
+                mClock.setPadding(mClockOriginalPaddingLeft, 0, 0, 0);
+                mLayoutClock.removeView(mClock);
+                mIconArea.addView(mClock);
+                mLayoutClock.setVisibility(View.GONE);
+                break;
+            case 1:
+                mClock.setGravity(Gravity.CENTER);
+                mClock.setLayoutParams(new LinearLayout.LayoutParams(
+                        LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+                mClock.setPadding(0, 0, 0, 0);
+                mIconArea.removeView(mClock);
+                mLayoutClock.addView(mClock);
+                mLayoutClock.setVisibility(View.VISIBLE);
+                break;
+                default:
+                    mClock.setGravity(Gravity.RIGHT | Gravity.CENTER_VERTICAL);
+                    mClock.setLayoutParams(new LinearLayout.LayoutParams(
+                            LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT));
+                    mClock.setPadding(mClockOriginalPaddingLeft, 0, 0, 0);
+                    mLayoutClock.removeView(mClock);
+                    mIconArea.addView(mClock);
+                    mLayoutClock.setVisibility(View.GONE);
+                    break;
+        }
+//        if (center) {
+//            mClock.setGravity(Gravity.CENTER);
+//            mClock.setLayoutParams(new LinearLayout.LayoutParams(
+//                    LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+//            mClock.setPadding(0, 0, 0, 0);
+//            mIconArea.removeView(mClock);
+//            mLayoutClock.addView(mClock);
+//            mLayoutClock.setVisibility(View.VISIBLE);
+//            log("Clock set to center position");
+//        } else {
+//            mClock.setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL);
+//            mClock.setLayoutParams(new LinearLayout.LayoutParams(
+//                    LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT));
+//            mClock.setPadding(mClockOriginalPaddingLeft, 0, 0, 0);
+//            mLayoutClock.removeView(mClock);
+//            mIconArea.addView(mClock);
+//            mLayoutClock.setVisibility(View.GONE);
+//            log("Clock set to normal position");
+//        }
+//
+//        mClockCentered = center;
+    }
+}
--- app/src/main/java/com/hieptran/devicesmanager/fragment/tweak/rom/StatusbarFragment.java
+++ app/src/main/java/com/hieptran/devicesmanager/fragment/tweak/rom/StatusbarFragment.java
@@ -8,32 +8,59 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.hieptran.devicesmanager.R;
+import com.hieptran.devicesmanager.common.root.CommandControl;
+import com.hieptran.devicesmanager.utils.Utils;
+import com.hieptran.devicesmanager.utils.tweak.CPU;
+
+import java.util.ArrayList;
+
+import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+import me.drakeet.materialdialog.MaterialDialog;
+
 /**
  * Created by hiepth on 30/03/2016.
  */
-public class StatusbarFragment extends Fragment implements IXposedHookLoadPackage{
+public class StatusbarFragment extends Fragment implements IXposedHookLoadPackage,IXposedHookInitPackageResources, AdapterView.OnItemSelectedListener{
+    Spinner mSpinnerClockPos;
+    protected boolean inhibit_spinner = true;
+
+    ArrayAdapter<String> mArrayAdapterClockPos;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.layout.rom_tweak_fragment, container, false);
+        mSpinnerClockPos = (Spinner) v.findViewById(R.id.clock_pos);
+        mArrayAdapterClockPos = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, new ArrayList<String>());
+        mArrayAdapterClockPos.add("Left");
+        mArrayAdapterClockPos.add("Center");
+        mArrayAdapterClockPos.add("Right");
+        mSpinnerClockPos.setAdapter(mArrayAdapterClockPos);
+        return v;
     }
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
             if(loadPackageParam.equals("com.android.systemui")) {
+                ClockPosition.initHandleLoadPackage(loadPackageParam.classLoader);
                 findAndHookMethod("com.android.systemui.statusbar.policy.Clock", loadPackageParam.classLoader, "updateClock", new XC_MethodHook() {
 
 
                     @Override
                     protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        Log.d("HiepTHB","set STTT");
+                        Log.d("HiepTHB", "set STTT");
                         TextView tv = (TextView) param.thisObject;
                         String text = tv.getText().toString();
                         tv.setText(text + " :)");
@@ -43,5 +70,58 @@ public class StatusbarFragment extends Fragment implements IXposedHookLoadPackag
 
                 }
             }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if (inhibit_spinner) {
+            inhibit_spinner = false;
+            type = Utils.getInt("clock_pos", 2, getContext());
+            //CPU.setGovernor(adap_avai_gov.getItem(position), getActivity());
+
+        } else {
+            Utils.saveInt("clock_pos", position, getContext());
+            type = position;
+
+//            mSflashDialog = new MaterialDialog(getContext())
+//                    .setTitle("Set Governor")
+//                    .setMessage("Change governor to " + ls_avai_gov.get(position) + "?")
+//                    .setPositiveButton("OK", new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            mState = 1;
+//                            setStaticView();
+//                            getActivity().invalidateOptionsMenu();
+//                            //   updateView();
+//                            UpdateView2();
+//                            mSflashDialog.dismiss();
+//
+//                        }
+//                    })
+//                    .setNegativeButton("CANCEL", new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            mSflashDialog.dismiss();
+//                        }
+//                    });
+//            mSflashDialog.show();
+//            if (mCores == CPU.getBigCore())
+//                CPU.setGovernor(adap_avai_gov.getItem(position), getActivity());
+//            else
+//                CPU.setGovernor(CommandControl.CommandType.CPU_LITTLE, adap_avai_gov.getItem(position), getActivity());
+
+        }
     }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+
+
+        }
+
+int type;
+    @Override
+    public void handleInitPackageResources(InitPackageResourcesParam initPackageResourcesParam) throws Throwable {
+        ClockPosition.initPackageResources(initPackageResourcesParam, type);
+    }
+}
 
